2b4d5db27cb69d9608cb7f7bce93a872014533c5
amir hossein bakhtiary davijani <amir.davijani.ext@adevinta.com>
2024-06-20 00:41:16 +0200

    first version is oooot

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+from datetime import datetime
+from typing import Optional, Annotated
+
+from fastapi import FastAPI, Cookie, Request, Header
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import uuid
+from pathlib import Path
+
+Path("data").mkdir(parents=True, exist_ok=True)
+
+class Bullshit(BaseModel):
+    text: str
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"check the docs for help"}
+
+
+@app.get("/health")
+async def health():
+    return {"I am good. Thank you!"}
+
+
+@app.post("/bullshitAI")
+async def bullshitAI(bs: Bullshit, request: Request, user_agent: Annotated[str | None, Header()] = None, cookie: Optional[str] = Cookie(None)):
+    if cookie is None:
+        cookie = str(uuid.uuid4())
+
+    with open(f"data/{datetime.now().timestamp()}{cookie}", 'w') as fp:
+        fp.write(f"{str(bs)} \n\n {request.client.host} \n\n {user_agent}")
+
+    response = JSONResponse(content={"message": "Great BS"})
+    response.set_cookie(key="session", value=cookie)
+
+    return {"I am good. Thank you!"}
+

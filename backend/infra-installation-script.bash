85aaeed8ed599d9973d2d7e317b7c4f3e37771f8
amir hossein bakhtiary davijani <amir.davijani.ext@adevinta.com>
2024-06-22 01:12:27 +0200

    backend uses port 8080

--- backend/infra-installation-script.bash
+++ backend/infra-installation-script.bash
@@ -1,2 +1,2 @@
-sudo apt install -y screen make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python3 python-is-python3 python3.12-venv
+sudo apt install -y screen make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python3 python-is-python3 python3.12-venv nginx
 

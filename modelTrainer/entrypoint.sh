2397c4267cb86b93dfb91d4f885ebff5ae7f3be5
amir hossein bakhtiary davijani <amir.davijani.ext@adevinta.com>
2024-08-31 21:55:50 +0200

    setup docker

--- modelTrainer/entrypoint.sh
+++ modelTrainer/entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd /app/src
-for f in $(find /inputs -maxdepth 1 -type f -printf "%f\n"); do
-    python main_cli.py /inputs/$f /outputs/$f
-done
+
+PYTHONPATH=/app/src uvicorn main:app --host 0.0.0.0

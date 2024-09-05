54701168dcad473d76eaeed198dc30ec6a6f3d53
amir hossein bakhtiary davijani <amir.davijani.ext@adevinta.com>
2024-09-05 23:11:43 +0200

    bring training script to python from ipynotebook

---
+++ bsDetector/src/train.py
@@ -0,0 +1,17 @@
+import transformers
+import torch
+import algorithm
+import xgboost
+
+from dataset import data, split
+
+model_id = "HuggingFaceTB/SmolLM-135M"
+
+smolLM_135_pipeline = transformers.pipeline(
+    "feature-extraction", model="HuggingFaceTB/SmolLM-135M", model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto",
+)
+
+theXGboostEmbeddingExtractorAlgorithm = algorithm.Algorithm(algorithm.PipelineAveragingEmbeddingExtractor(smolLM_135_pipeline), xgboost.XGBClassifier())
+algorithm.runExperiment(theXGboostEmbeddingExtractorAlgorithm, algorithm.Dataset(data, split), algorithm.Accuracy())
+
+theXGboostEmbeddingExtractorAlgorithm.save_to_directory("model")
